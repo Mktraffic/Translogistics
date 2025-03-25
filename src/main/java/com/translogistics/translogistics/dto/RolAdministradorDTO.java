bab5d36f090845c64ba04e93f454079f1964a7e5
rojasoscar752 <rojasoscar752@gmail.com>
2025-03-24 20:32:36 -0500

    feat: Agregar rol admin DTO

---
+++ src/main/java/com/translogistics/translogistics/dto/RolAdministradorDTO.java
@@ -0,0 +1,11 @@
+package com.translogistics.translogistics.dto;
+
+public class RolAdministradorDTO extends RolDTO {
+    public RolAdministradorDTO(Long id, String nombreRol) {
+        super(id, nombreRol);
+    }
+
+    public RolAdministradorDTO() {
+        //TODO Auto-generated constructor stub
+    }
+}
