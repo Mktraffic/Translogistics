806281179d8b157adf3002eadb850a02beef3bb5
GabrielNinoA <gabriel.nino02@uptc.edu.co>
2025-03-24 19:35:15 -0500

    feat: Agregar base proyecto

---
+++ src/test/java/com/translogistics/translogistics/TranslogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translogistics.translogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
